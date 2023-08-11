745f184b4cb7a01d0f2e3044797f98179dfd8e06
badeska <iyanufeyintoluwa@gmail.com>
2023-08-10 21:34:54 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+printf("with proper programmar, but the outcome is a piece of art, \n");
+Return (0);
+}
