6cdd45d520c1954b2199085790cd08012c23558c
badeska <iyanufeyintoluwa@gmail.com>
2023-08-14 15:05:49 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,17 +6,9 @@
  */
 int main(void)
 {
-	putchar(48); /* ASCII value of '0' */
-	putchar(49); /* ASCII value of '1' */
-	putchar(50); /* ASCII value of '2' */
-	putchar(51); /* ASCII value of '3' */
-	putchar(52); /* ASCII value of '4' */
-	putchar(53); /* ASCII value of '5' */
-	putchar(54); /* ASCII value of '6' */
-	putchar(55); /* ASCII value of '7' */
-	putchar(56); /* ASCII value of '8' */
-	putchar(57); /* ASCII value of '9' */
-	putchar('\n');
+	putchar('0');
+	putchar(10);
+/* ASCII value of newline character */
 
 	return (0);
 }
