f12f9d6a5b45984a36b1b02b44a40c1d0a24be93
badeska <iyanufeyintoluwa@gmail.com>
2023-08-14 14:36:05 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int first_number;
+	int second_number;
+
+	for (first_number = 0; first_number < 100; first_number++)
+	{
+		for (second_number = 0; second_number < 100; second_number++)
+		{
+			if (first_number <= second_number)
+			{
+				putchar((first_number / 10) + '0');
+				putchar((first_number % 10) + '0');
+				putchar(' ');
+				putchar((second_number / 10) + '0');
+				putchar((second_number % 10) + '0');
+
+				if (first_number < 99 || second_number < 99)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
