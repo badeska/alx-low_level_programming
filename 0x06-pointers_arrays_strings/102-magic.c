aa731a490a403fd8dbc61a2796b271e82c2b4711
badeska <iyanufeyintoluwa@gmail.com>
2023-08-23 17:21:10 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
+
 /**
- * main - initializes an integer array 'a' and an integer 'n'.
- * It then calculates a memory address 'p' for 'n' and updates
- * the value at the address 'p + 5' to be 98. Afterward, it prints
- * the value of 'a[2]' and returns 0 to indicate successful completion.
- *
- * Return: Always 0.
- */
+ * Description: main - addition of a line of code.
+ * Return: Always success
+*/
+
 int main(void)
 {
 int n;
@@ -16,6 +14,16 @@ int *p;
 a[2] = 1024;
 p = &n;
 *(p + 5) = 98;
+/*
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
+*/
+;
+/* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
